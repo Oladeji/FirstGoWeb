2ed8bbf66f27fcb8569c39f6471f13ef6b2d9f5f
Oladeji Akomolafe <Akomspatrick@hotmail.com>
2020-02-16 00:16:20 +0100

    newbranch

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import ("fmt"
+        "net/http")
 
+func handleFunc(w http.ResponseWriter , r *http.Request ){
+fmt.Println("Hello World- lister")
+fmt.Fprint(w, "<h1>Welcome to world wide be</h1>")
+}
 func main() {
-	fmt.Println("Hello World- And everybody")
+	http.HandleFunc("/",handleFunc)
+	http.ListenAndServe(":3000",nil)
+	//fmt.Println("Hello World- And everybody")
 }
